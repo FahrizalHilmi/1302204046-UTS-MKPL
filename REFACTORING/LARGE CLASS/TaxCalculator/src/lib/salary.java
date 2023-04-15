26c218636ad5f5890b435f0009be8d4db453a221
rizal <fahrizalhilmi2002@gmail.com>
2023-04-16 00:39:30 +0700

    memisahkan kelas employee kedalam person salary dan employee

---
+++ REFACTORING/LARGE CLASS/TaxCalculator/src/lib/salary.java
@@ -0,0 +1,40 @@
+package lib;
+
+public class Salary {
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+
+    public Salary(int monthlySalary, int otherMonthlyIncome, int annualDeductible) {
+        this.monthlySalary = monthlySalary;
+        this.otherMonthlyIncome = otherMonthlyIncome;
+        this.annualDeductible = annualDeductible;
+    }
+
+    // Getter dan setter untuk atribut monthlySalary
+    public int getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(int monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    // Getter dan setter untuk atribut otherMonthlyIncome
+    public int getOtherMonthlyIncome() {
+        return otherMonthlyIncome;
+    }
+
+    public void setOtherMonthlyIncome(int otherMonthlyIncome) {
+        this.otherMonthlyIncome = otherMonthlyIncome;
+    }
+
+    // Getter dan setter untuk atribut annualDeductible
+    public int getAnnualDeductible() {
+        return annualDeductible;
+    }
+
+    public void setAnnualDeductible(int annualDeductible) {
+        this.annualDeductible = annualDeductible;
+    }
+}
